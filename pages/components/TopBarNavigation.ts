73f4bcfe16ab34006f1b966518322b625cd65b19
Chau Sen <chausen0611@gmail.com>
2025-11-17 13:16:25 +0700

    Update code 17/11

--- pages/components/TopBarNavigation.ts
+++ pages/components/TopBarNavigation.ts
@@ -24,18 +24,12 @@ export class TopBarNavigation extends BasePage {
         await this.click(this.lnkRegister);
     }
 
-    // async openMenuItem(item: string) {
-    //     // String menuItemXpath = String.format(menuItem, item); // Java
-    //     let menuItemXpath: string = this.menuItem.replace('%s', item); // String locator
-    //     // this.page.locator(menuItemXpath).click(); // Locator tuong ung voi WebElement
-    //     this.click(menuItemXpath); //goi click cua BasePage
     async openMenuItem(item: string) {
         // const menuItemXpath = this.menuItem.replace('%s', item);
         const element = this.page.locator('//*[@id="root"]/div/div[1]/header/div/div/div[2]/a[2]');
         await this.page.waitForTimeout(5000)
-        // await element.waitFor({ state: 'visible', timeout: 20000 }); // đảm bảo element hiển thi
         await element.click();
     }
-    // await this.click(this.page.locator(menuItemXpath));
+   
 }
 
--- tests/Book_ticket/book_Ticket_From_Cum_Rap_Page.spec.ts
+++ tests/Book_ticket/book_Ticket_From_Cum_Rap_Page.spec.ts
@@ -1,14 +1,28 @@
-import test from "@playwright/test";
+import test, { expect } from "@playwright/test";
 import { TopBarNavigation } from "../../pages/components/TopBarNavigation";
 import { HomePage } from "../../pages/menu_pages/HomePage";
+import { CumRapPage } from "../../pages/menu_pages/CumRapPage";
 
 test('Book Ticket From Cum Rap Menu', async ({ page }) => {
     const topBarNavigation = new TopBarNavigation(page);
     const homePage = new HomePage(page);
+    const cumRapPage = new CumRapPage(page);
 
+    // Step 1: Open Homepage va open menu Cụm Rạp
     await homePage.navigateTo("https://demo1.cybersoft.edu.vn/");
     await topBarNavigation.openMenuItem('Cụm Rạp');
 
 
+    await cumRapPage.selectClusterByIndex(0);
 
-})
+
+    // Step 3: Verify danh sách phim hiển thị
+    await cumRapPage.selectRandomTimeSlot();
+
+    await expect(page).toHaveURL(/purchase/);
+});
+
+//  Step 4: Chọn khung giờ của 1 phim
+
+
+//  Step 5: navigate to SelectSeatPage
